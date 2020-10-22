80621ae71715a7ba8250786dc1bdfa3fb1bba50e
Gyuhyeon Sim <wallys0213@gmail.com>
2020-10-22 09:29:09 +0900

    201022 | update KoreanInputForm.vue

--- src/components/KoreanInputForm.vue
+++ src/components/KoreanInputForm.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <h2>v-model = 'v-on:input' + 'v-bind:input'의 조합</h2>
+  <section>
+    <h2>input, textarea에 한글 입력 시 v-model 구현 방법</h2>
     <input @input="korTyping" :value="korMessage" type="text">
     <p>메세지: {{ korMessage }}</p>
+    <!-- input, textarea 태그 v-model = 'v-on:input' + 'v-bind:value'의 조합 -->
     <textarea @input="korTypingText" :value="korText" type="text" class="kor-textarea"></textarea>
     <p>메세지: {{ korText }}</p>
-  </div>
+  </section>
 </template>
 
 <script>
